37f97be8e53e505d567a027cbee4ad743d7d734d
stakahashi <kyuuketukishouta@gmail.com>
2024-09-27 16:51:31 +0900

    feat: データベースからタスクを取得してWebページに表示

--- server/webPage/script/index.js
+++ server/webPage/script/index.js
@@ -2,8 +2,38 @@
 window.onload = function loadTasks(){
     fetch('http://13.231.30.42:3000')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data[0]);
+        // viewTask(obj.task);
+        for (let i = 0 ; i < data.length ; i++){
+            viewTask(data[i].task);
+        }
+    })
     .catch(error => console.error('Error:', error));
+}
+
+// タスク追加
+function viewTask(value){
+    // 要素取得
+    const taskList = document.getElementById("taskList");
+    const addTask = document.getElementById("taskContent");
+
+    // 追加するHTML要素(タスク)の作成
+    let div = document.createElement("div");
+    let checkbox = document.createElement("input");
+    let p = document.createElement("p");
+    checkbox.setAttribute("type","checkbox");
+    checkbox.setAttribute("onclick","inputCompleteButton()");
+    p.append(value);
+    div.setAttribute("class","task");
+    div.append(checkbox);
+    div.append(p);
+    // スタイル設定
+    div.style.display = "flex";
+
+    // タスクリストに追加
+    taskList.append(div);
+    console.log("add task!");
 }
 
 // タスク追加
